7f0507a61b23fa9f748befb8ae84eabbb0350399
Victor <bvictor@ethz.ch>
2022-12-08 16:31:30 +0100

    added few figures with runtime to answer benno

--- figure/complexity_vs_error.jl
+++ figure/complexity_vs_error.jl
@@ -23,8 +23,10 @@ path_results = "../results/2022-10-04/explo_param_MLP_T=0.5_explo_K_M_4/"
 dict_results_MLP =load(path_results*"dict_results_MLP_param_explo.jld2")
 df_all_params_MLP = vcat([dict_results_MLP[k]["df_ds"] for k in keys(dict_results_MLP)]...)
 
-fig, axs = subplots(1,2)
-
+#######################################
+# Plotting only for a single parameter
+#######################################
+fig, axs = subplots(1,2, figsize=(6,3))
 ## time vs error
 ax = axs[1]
 # DS
@@ -34,22 +36,85 @@ ax.errorbar(data[:,6],
             yerr=(data."Std. dev. error"), 
             fmt="o", 
             color="tab:blue",
-            ecolor="tab:blue", elinewidth=3, capsize=0)
+            ecolor="tab:blue", elinewidth=3, capsize=0, label="ML-based approx. method")
 display(fig)
 ax.set_xlabel("Time (s)")
 ax.set_ylabel(L"$L^1-$approx. error")
 # ax.set_xscale("log")
 display(fig)
 # MLP
+data = dict_results_MLP["explo_L"]["df_ds"]
 ax = axs[2]
-ax.errorbar(df_all_params_MLP[:,6], 
-            log.(df_all_params_MLP[:,4]), 
-            yerr=log.(df_all_params_MLP."Std. dev. error"), 
+ax.errorbar(data[:,6], 
+            data[:,4], 
+            yerr=data."Std. dev. error", 
             fmt="o", 
             color="tab:red",
-            ecolor="tab:red", elinewidth=3, capsize=0)
+            ecolor="tab:red", elinewidth=3, capsize=0, label="MLP approx. method")
+display(fig)
+ax.set_xlabel("Time (s)")
+# ax.set_ylabel(L"$L^1-$approx. error")
+ax.set_xscale("log")
+fig.legend(loc="lower center", bbox_to_anchor=(0.55, 1.,))
+fig.tight_layout()
+display(fig)
+fig.savefig("time_vs_l1_err.pdf", dpi=100)
+
+#######################################
+# Plotting for all parameters where d and T are similar
+#######################################
+fig, axs = subplots(1,2, figsize=(6,3))
+## time vs error
+ax = axs[1]
+# DS
+data = vcat(dict_results_DS["explo_batch_size"]["df_ds"],dict_results_DS["explo_K"]["df_ds"])
+ax.errorbar(data[:,6], 
+            (data[:,4]), 
+            yerr=(data."Std. dev. error"), 
+            fmt="o", 
+            color="tab:blue",
+            ecolor="tab:blue", elinewidth=3, capsize=0, label="ML-based approx. method")
 display(fig)
 ax.set_xlabel("Time (s)")
 ax.set_ylabel(L"$L^1-$approx. error")
-ax.set_xscale("log")
+# ax.set_xscale("log")
 display(fig)
+# MLP
+data = df_all_params_MLP
+ax = axs[2]
+ax.errorbar(data[:,6], 
+            data[:,4], 
+            yerr=data."Std. dev. error", 
+            fmt="o", 
+            color="tab:red",
+            ecolor="tab:red", elinewidth=3, capsize=0, label="MLP approx. method")
+display(fig)
+ax.set_xlabel("Time (s)")
+# ax.set_ylabel(L"$L^1-$approx. error")
+ax.set_xscale("log")
+fig.legend(loc="lower center", bbox_to_anchor=(0.55, 1.,))
+fig.tight_layout()
+display(fig)
+fig.savefig("time_vs_l1_err_bis.pdf", dpi=100)
+
+
+#######################################
+# Plotting for all parameters where d and T are similar
+# only for MLP method
+#######################################
+fig, ax = subplots(1, figsize=(3,3))
+data = df_all_params_MLP
+ax.errorbar(data[:,6], 
+            data[:,4], 
+            yerr=data."Std. dev. error", 
+            fmt="o", 
+            color="tab:red",
+            ecolor="tab:red", elinewidth=3, capsize=0, label="MLP approx. method")
+display(fig)
+ax.set_xlabel("Time (s)")
+# ax.set_ylabel(L"$L^1-$approx. error")
+ax.set_xscale("log")
+fig.legend(loc="lower center", bbox_to_anchor=(0.55, 1.,))
+fig.tight_layout()
+display(fig)
+fig.savefig("time_vs_l1_err_MLP.pdf", dpi=100)
